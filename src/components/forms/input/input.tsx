1ddbf3724da9311d0f9891a54f79dbbf52685a6c
Emeka Uja <ujadninth@gmail.com>
2022-05-13 10:38:48 +0100

    modified input styles

--- src/components/forms/input/input.tsx
+++ src/components/forms/input/input.tsx
@@ -2,32 +2,13 @@ import * as React from 'react';
 
 import classNames from 'classnames';
 
-const baseClass = classNames(
-  'flex',
-  'items-center',
-  'border',
-  'justify-between',
-  'rounded-md',
-  'py-2.5',
-  'px-4',
-  'space-x-2',
-  'bg-black'
-);
-
 // full width
 const fullWidth = classNames('w-full');
-
-// outline variant
-const regularClass = classNames(
-  'border-gray-500',
-  'focus-within:border-gray-400',
-  'focus-within:shadow'
-);
 
 // bare-bones variant
 const bareBonesVariant = classNames(
   'text-sm',
-  'text-white',
+  'text-black',
   'border-none',
   'rounded-none',
   'py-0',
@@ -35,8 +16,8 @@ const bareBonesVariant = classNames(
   'focus:outline-0',
   'focus:ring-0',
   'place-holder:text-gray-900',
-  'flex-1',
-  'bg-black'
+  'flex-1'
+  // 'bg-black'
 );
 
 export interface IInputProps extends React.ComponentPropsWithRef<'input'> {
@@ -47,21 +28,11 @@ export interface IInputProps extends React.ComponentPropsWithRef<'input'> {
 
 // eslint-disable-next-line react/display-name
 export const Input = React.forwardRef<HTMLInputElement, IInputProps>(
-  ({ rightElement, leftElement, isFullWidth = true, ...props }, ref) => {
-    const inputClass = classNames(baseClass, regularClass, {
-      [fullWidth]: isFullWidth,
-    });
-
+  ({ isFullWidth = true, ...props }, ref) => {
     const inputBareBoneClass = classNames(bareBonesVariant, {
       [fullWidth]: isFullWidth,
     });
 
-    return (
-      <div className={inputClass}>
-        {!!leftElement && leftElement}
-        <input ref={ref} {...props} className={inputBareBoneClass} />
-        {!!rightElement && rightElement}
-      </div>
-    );
+    return <input ref={ref} {...props} className={inputBareBoneClass} />;
   }
 );
