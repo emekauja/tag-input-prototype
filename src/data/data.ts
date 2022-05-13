53a3d6616c02d2388a27be54eadd71c35cb2142b
Emeka Uja <ujadninth@gmail.com>
2022-05-13 10:40:34 +0100

    added fake data

---
+++ src/data/data.ts
@@ -0,0 +1,7 @@
+export const titleOptions = [
+  { label: 'English', value: 'english' },
+  { label: 'French', value: 'french' },
+  { label: 'Arabic', value: 'arabic' },
+  { label: 'Spanish', value: 'spanish' },
+  { label: 'Portuguese', value: 'portuguese' },
+];
